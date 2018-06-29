e994d3ccd6eeaa684c1a015a36925f6350c61efe
Christopher Beards <beardscm@gmail.com>
2018-06-29 13:51:46 -0400

    Added MongoDB

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 // * Initialze a Variable to Express
 const app = express();
+
+// * DB Config
+const db = require('./config/keys').mongoURI;
+
+// * Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 // * Test Route
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+// * Port
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
