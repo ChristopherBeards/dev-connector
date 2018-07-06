c67c0b16b85ce06cfcbb7baf0129b288dfac1671
Christopher Beards <beardscm@gmail.com>
2018-07-06 16:02:46 -0400

    User Validation Works

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
 
 // * Load User Model
 const User = require('../../models/User');
@@ -62,12 +65,42 @@ router.post('/login', (req, res) => {
     // Check Password
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        res.json({ msg: 'Success' });
+        // User Matched
+        // Create JWT Payload
+        const payload = { id: user.id, name: user.name, avatar: user.avatar };
+
+        // Sign Token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: 'Bearer ' + token,
+            });
+          },
+        );
       } else {
         return res.status(400).json({ password: 'Password incorrect' });
       }
     });
   });
 });
+
+// * @route   GET api/users/current
+// * @desc    Return current user
+// * @access  Private
+router.get(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email,
+    });
+  },
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -22,10 +23,11 @@ mongoose
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-// * Test Route
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+// * Passport Middleware
+app.use(passport.initialize());
+
+// * Passport Config
+require('./config/passport.js')(passport);
 
 // * Port
 const port = process.env.PORT || 5000;
