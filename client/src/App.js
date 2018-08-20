a4760a879a63e3c1b9b614911412db4e4e0a73bc
Christopher Beards <beardscm@gmail.com>
2018-08-20 07:53:58 -0400

    Navbar, Landing, Footer; initial

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+
 import './App.css';
+
+// * Components
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Welcome to the party</h1>
+        <Navbar />
+        <Landing />
+        <Footer />
       </div>
     );
   }
