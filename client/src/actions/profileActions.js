ad80fcb9ed73cf88378d6935d32e7bc030b83fa0
Christopher Beards <beardscm@gmail.com>
2018-08-30 17:46:50 -0400

    Added Input Groups for Create Profile
    Added create profile action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -24,6 +24,18 @@ export const getCurrentProfile = () => dispatch => {
       }),
     );
 };
+// * Create Profile
+export const createProfile = (profileData, history) => dispatch => {
+  axios
+    .post('/api/profile', profileData)
+    .then(res => history.push('/dashboard'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      }),
+    );
+};
 
 // * Profile loading
 export const setProfileLoading = () => {
