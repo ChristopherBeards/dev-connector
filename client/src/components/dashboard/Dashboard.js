e8ec42b7945ccfeaa268979826aa11713e63bbbe
Christopher Beards <beardscm@gmail.com>
2018-09-03 17:20:42 -0400

    Education; Added; Implemented Delete, added a go back button

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,6 +6,7 @@ import { getCurrentProfile, deleteAccount } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
 import ProfileActions from './ProfileActions';
 import Experience from './Experience';
+import Education from './Education';
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -36,6 +37,7 @@ class Dashboard extends Component {
             </p>
             <ProfileActions />
             <Experience experience={profile.experience} />
+            <Education education={profile.education} />
             <div style={{ marginBottom: '60px' }} />
             <button onClick={this.onDeleteClick} className="btn btn-danger">
               Delete Account
@@ -55,6 +57,7 @@ class Dashboard extends Component {
         );
       }
     }
+
     return (
       <div className="dashboard">
         <div className="container">
