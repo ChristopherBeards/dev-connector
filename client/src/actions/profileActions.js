40852eb4085be4afb3a7c005bf7a711e20238639
Christopher Beards <beardscm@gmail.com>
2018-09-03 17:09:49 -0400

    Experience; Delete Implemented

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -64,6 +64,24 @@ export const addEducation = (eduData, history) => dispatch => {
     );
 };
 
+// * Delete Experience
+export const deleteExperience = id => dispatch => {
+  axios
+    .delete(`/api/profile/experience/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data,
+      }),
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      }),
+    );
+};
+
 // * Delete Account and Profile
 export const deleteAccount = () => dispatch => {
   if (window.confirm('Are you sure? This can NOT be undone!')) {
--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
 import Moment from 'react-moment';
+import { deleteExperience } from '../../actions/profileActions';
 
 class Experience extends Component {
+  onDeleteClick = id => {
+    this.props.deleteExperience(id);
+  };
+
   render() {
     const experience = this.props.experience.map(exp => (
       <tr key={exp._id}>
@@ -19,7 +23,12 @@ class Experience extends Component {
           )}
         </td>
         <td>
-          <button className="btn btn-danger">Delete</button>
+          <button
+            className="btn btn-danger"
+            onClick={this.onDeleteClick.bind(this, exp._id)}
+          >
+            Delete
+          </button>
         </td>
       </tr>
     ));
@@ -43,4 +52,11 @@ class Experience extends Component {
   }
 }
 
-export default connect(null)(withRouter(Experience));
+Experience.propTypes = {
+  deleteExperience: PropTypes.func.isRequired,
+};
+
+export default connect(
+  null,
+  { deleteExperience },
+)(Experience);
