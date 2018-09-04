70a26953bb316e6e3f65e798b13984eed24a77af
Christopher Beards <beardscm@gmail.com>
2018-09-04 17:52:11 -0400

    ProfileAbout; implemented

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -6,36 +6,37 @@ class ProfileAbout extends Component {
   render() {
     const { profile } = this.props;
 
+    // Get first name
+    const firstName = profile.user.name.trim().split(' ')[0];
+
+    // Skills list
+    const skills = profile.skills.map((skill, index) => (
+      <div key={index} className="p-3">
+        <i className="fa fa-check" />
+        {skill}
+      </div>
+    ));
+
     return (
       <div className="row">
         <div className="col-md-12">
           <div className="card card-body bg-light mb-3">
-            <h3 className="text-center text-info">John's Bio</h3>
+            <h3 className="text-center text-info">
+              {firstName}
+              's Bio
+            </h3>
             <p className="lead">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Provident fuga cum necessitatibus blanditiis vel, officia facere
-              porro esse numquam assumenda doloremque saepe aliquam nemo
-              excepturi aliquid maiores! Excepturi, libero repudiandae.
+              {isEmpty(profile.bio) ? (
+                <span>{profile.first}</span>
+              ) : (
+                <span>{firstName} does not have a bio</span>
+              )}
             </p>
             <hr />
             <h3 className="text-center text-info">Skill Set</h3>
             <div className="row">
               <div className="d-flex flex-wrap justify-content-center align-items-center">
-                <div className="p-3">
-                  <i className="fa fa-check" /> HTML
-                </div>
-                <div className="p-3">
-                  <i className="fa fa-check" /> CSS
-                </div>
-                <div className="p-3">
-                  <i className="fa fa-check" /> JavaScript
-                </div>
-                <div className="p-3">
-                  <i className="fa fa-check" /> Python
-                </div>
-                <div className="p-3">
-                  <i className="fa fa-check" /> C#
-                </div>
+                {skills}
               </div>
             </div>
           </div>
