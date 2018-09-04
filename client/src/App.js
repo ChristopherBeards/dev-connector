91b6e13eca26386a2a84937e6cef59c45b38245a
Christopher Beards <beardscm@gmail.com>
2018-09-04 10:48:54 -0400

    Added profile route

--- client/src/App.js
+++ client/src/App.js
@@ -26,6 +26,7 @@ import EditProfile from './components/edit-profile/EditProfile';
 import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 
 // * Check for token
 if (localStorage.jwtToken) {
@@ -59,6 +60,7 @@ class App extends Component {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:handle" component={Profile} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
