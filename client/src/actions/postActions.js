9faaf6f520a79089a9912c762b80d671bffc2d9f
Christopher Beards <beardscm@gmail.com>
2018-09-07 15:38:54 -0400

    addComment Action Created

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -115,3 +115,21 @@ export const deletePost = id => dispatch => {
       }),
     );
 };
+
+// Add Comment
+export const addComment = (postId, commentData) => dispatch => {
+  axios
+    .post(`/api/posts/comment/${postId}`, commentData)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data,
+      }),
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      }),
+    );
+};
