0ddfcb74fd395d4b3d543c20b5191db1e23648f0
Christopher Beards <beardscm@gmail.com>
2018-09-07 15:30:12 -0400

    Conditional Rendering of postActions

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import PostItem from '../posts/PostItem';
 import Spinner from '../common/Spinner';
 import { getPost } from '../../actions/postActions';
 
@@ -10,10 +12,32 @@ class Post extends Component {
   }
 
   render() {
+    const { post, loading } = this.props.post;
+    let postContent;
+
+    if (post === null || loading || Object.keys(post).length === 0) {
+      postContent = <Spinner />;
+    } else {
+      postContent = (
+        <div>
+          <PostItem post={post} showActions={false} />
+        </div>
+      );
+    }
+
     return (
-      <dir>
-        <h1>POST</h1>
-      </dir>
+      <div className="post">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <Link to="/feed" className="btn btn-light mb-3">
+                Back To Feed
+              </Link>
+              {postContent}
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
