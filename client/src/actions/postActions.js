67701ab4987ea342a61fce37091a5e5c21eeb322
Christopher Beards <beardscm@gmail.com>
2018-09-07 15:57:15 -0400

    deleteComment Action Created

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -133,3 +133,21 @@ export const addComment = (postId, commentData) => dispatch => {
       }),
     );
 };
+
+// Delete Comment
+export const deleteComment = (postId, commentId) => dispatch => {
+  axios
+    .delete(`/api/posts/comment/${postId}/${commentId}`)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data,
+      }),
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      }),
+    );
+};
