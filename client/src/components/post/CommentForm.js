5493b14023b3d792b12945c49902aee8a7661769
Christopher Beards <beardscm@gmail.com>
2018-09-07 15:36:48 -0400

    CommentForm created

---
+++ client/src/components/post/CommentForm.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
+import { addComment } from '../../actions/postActions';
+
+class CommentForm extends Component {
+  state = {
+    text: '',
+    errors: {},
+  };
+
+  componentWillReceiveProps(newProps) {
+    if (newProps.errors) {
+      this.setState({ errors: newProps.errors });
+    }
+  }
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    const { user } = this.props.auth;
+    const { postId } = this.props;
+
+    const newComment = {
+      text: this.state.text,
+      name: user.name,
+      avatar: user.avatar,
+    };
+
+    this.props.addComment(postId, newComment);
+    this.setState({ text: '' });
+  };
+
+  render() {
+    const { errors } = this.state;
+
+    return (
+      <div className="post-form mb-3">
+        <div className="card card-info">
+          <div className="card-header bg-info text-white">
+            Leave a comment...
+          </div>
+          <div className="card-body">
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <TextAreaFieldGroup
+                  className="form-control form-control-lg"
+                  placeholder="Reply to post"
+                  name="text"
+                  value={this.state.text}
+                  onChange={this.onChange}
+                  error={errors.text}
+                />
+              </div>
+              <button type="submit" className="btn btn-dark">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+CommentForm.propTypes = {
+  addComment: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  postId: PropTypes.string.isRequired,
+  errors: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+  errors: state.errors,
+  auth: state.auth,
+});
+
+export default connect(
+  mapStateToProps,
+  { addComment },
+)(CommentForm);
