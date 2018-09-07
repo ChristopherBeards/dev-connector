c373f6570a58149ea2200d2f56993d647bf505d9
Christopher Beards <beardscm@gmail.com>
2018-09-07 15:20:52 -0400

    Post; Created

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Spinner from '../common/Spinner';
+import { getPost } from '../../actions/postActions';
 
 class Post extends Component {
+  componentDidMount() {
+    this.props.getPost(this.props.match.params.id);
+  }
+
   render() {
     return (
       <dir>
@@ -10,4 +18,16 @@ class Post extends Component {
   }
 }
 
-export default Post;
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+  post: state.post,
+});
+
+export default connect(
+  mapStateToProps,
+  { getPost },
+)(Post);
