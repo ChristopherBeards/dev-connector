baedf222db44fd25ff5fbac826f11a37321085eb
Christopher Beards <BeardsCM@gmail.com>
2018-09-25 22:22:59 -0400

    Refactored store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,22 +1,22 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-
-// * Import the rootReducer
-import rootReducer from './reducers';
-
-const initialState = {};
-
-const middleware = [thunk];
-
-// * Create store
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
-);
-
-export default store;
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers'
+ 
+const initialState = {}
+ 
+const middleware = [thunk]
+ 
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose
+ 
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware),
+  // other store enhancers if any
+)
+const store = createStore(rootReducer, initialState, enhancer)
+ 
+export default store
