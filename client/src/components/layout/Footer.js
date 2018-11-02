8d1cd7e36079b1ab8f18951bdf8fadfeb7151873
Christopher Beards <BeardsCM@gmail.com>
2018-11-02 14:14:33 -0400

    Footer; modified to be sticky

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => {
   return (
-    <footer className="bg-dark text-white mt-5 p-4 text-center">
+    <footer className="bg-dark text-white mt-5 p-4 text-center" style={{position: "fixed", bottom: "0", width: "100%"}}>
       Copyright &copy; {new Date().getFullYear()} Dev Connector
     </footer>
   );
