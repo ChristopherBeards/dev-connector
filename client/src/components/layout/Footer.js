ee406a0149a51938aa95391defaa7010b864380a
Christopher Beards <BeardsCM@gmail.com>
2018-11-02 14:27:37 -0400

    Footer; fixed overlap of content

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => {
   return (
-    <footer className="bg-dark text-white mt-5 p-4 text-center" style={{position: "fixed", bottom: "0", width: "100%"}}>
+    <footer className="bg-dark text-white mt-5 p-4 text-center fixed-bottom">
       Copyright &copy; {new Date().getFullYear()} Dev Connector
     </footer>
   );
