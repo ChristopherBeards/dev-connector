e5dfbc03b389def1fb58f60d7b5118da832dd3b5
Christopher Beards <BeardsCM@gmail.com>
2018-11-04 19:55:34 -0500

    types; Edit Post type created

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -11,3 +11,4 @@ export const GET_POSTS = 'GET_POSTS';
 export const GET_POST = 'GET_POST';
 export const ADD_POST = 'ADD_POST';
 export const DELETE_POST = 'DELETE_POST';
+export const EDIT_POST = 'EDIT_POST';
